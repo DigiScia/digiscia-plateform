4345d2f6a63d8731d4965e5012a3045efcb79ae9
fidelis <yonlifidelis2@gmail.com>
2025-11-21 23:38:15 +0100

    Deployement test 1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000"; // L'URL de ton backend Django
+// const API_URL = "http://127.0.0.1:8000"; // L'URL de ton backend Django
+const API_URL = "https://rim-seed-relief-pointer.trycloudflare.com/"; // L'URL de ton backend Django
+
 
 const api = axios.create({
   baseURL: API_URL,
